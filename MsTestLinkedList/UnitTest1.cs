9c9cb8e6b54237e05f9bca885e71891322633978
kretika arora <kretikaarora@gmail.com>
2020-10-23 19:56:26 +0530

    [Kretika Arora]Creating a simple linkedlist

--- MsTestLinkedList/UnitTest1.cs
+++ MsTestLinkedList/UnitTest1.cs
@@ -1,14 +1,11 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using DataStructures_LinkedList
+using DataStructures_LinkedList;
 
 namespace MsTestLinkedList
 {
     [TestClass]
     public class UnitTest1
     {
-        [TestMethod]
-        public void TestMethod1()
-        {
-        }
+        
     }
 }
